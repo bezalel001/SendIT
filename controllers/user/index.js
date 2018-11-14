83b1cc6a6c9c9d8990a4a7bf9a8809fd343ad79a
bezalel001 <mickeybezalel@gmail.com>
2018-11-14 01:59:34 +0100

    Add login for user authentication

---
+++ controllers/user/index.js
@@ -0,0 +1,82 @@
+import moment from 'moment';
+import querySendItDb from '../db';
+
+const userController = {
+
+  // create a user account
+  async create(req, res) {
+    if (!req.body.firstName || !req.body.lastName) {
+      return res.status(400).json({ message: 'Missing name' });
+    }
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).json({ message: 'Missing username or password' });
+    }
+    if (!req.body.email) {
+      return res.status(400).json({ message: 'Missing email' });
+    }
+
+    // TODO: validate email
+    // TODO: hash password
+
+    const queryText = `
+      INSERT INTO user_account(first_name, last_name, other_names, email, password, username, registered, is_admin)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8) returning * `;
+
+    const values = [
+      req.body.firstName,
+      req.body.lastName,
+      req.body.otherNames,
+      req.body.email,
+      req.body.password, // hash(req.body.password)
+      req.body.username,
+      moment((new Date())),
+      req.body.isAdmin,
+    ];
+
+    try {
+      const { rows } = await querySendItDb(queryText, values);
+      // TODO: define token here
+      return res.status(201).json({ message: 'User created' });
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).json({ message: 'User with that email or username already exists' });
+      }
+
+      return res.status(400).json({ error: error.message });
+    }
+  },
+
+  // login user
+  async login(req, res) {
+    console.log('Req.body: ', req.body);
+    console.log('Email: ', req.body.email);
+    console.log('Password: ', req.body.password);
+    if (!req.body.email) {
+      return res.status(400).json({ message: 'Missing email' });
+    }
+    if (!req.body.password) {
+      return res.status(400).json({ message: 'Password missing!' });
+    }
+    // check if email is valid
+    const queryText = 'SELECT * FROM user_account WHERE email = $1';
+
+    try {
+      const { rows } = await querySendItDb(queryText, [req.body.email]);
+      if (!rows[0]) {
+        return res.status(400).json({ message: 'No account with the email you provided!' });
+      }
+      // compare password:TODO hash and compare
+      if (rows[0].password !== req.body.password) {
+        return res.status(400).json({ message: 'Invalid password' });
+      }
+      // TODO: generrate token
+      return res.status(200).json({ messsage: 'Login successful' });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  },
+
+
+};
+
+export default userController;
---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import userController from './controllers/user';
+
+
+const PORT = 3000;
+
+const app = express();
+app.set('json spaces', 4);
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to sendIt application' });
+});
+
+// create user account
+app.post('/auth/signup', (req, res) => {
+  userController.create(req, res);
+});
+
+// login user
+app.post('/auth/login', (req, res) => {
+  userController.login(req, res);
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on Port -- ${PORT}`);
+});
