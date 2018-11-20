d257c3266e30af939eb40712785173c01037869d
bezalel001 <mickeybezalel@gmail.com>
2018-11-20 11:09:58 +0100

    Add api documenation

---
+++ controllers/auth/index.js
@@ -0,0 +1,123 @@
+import moment from 'moment';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import querySendItDb from '../db';
+
+dotenv.config();
+
+const authController = {
+
+  // Create a user account
+  async createUserAccount(req, res) {
+    if (!req.body.firstName || !req.body.lastName) {
+      return res.status(400).json({ message: 'Missing name' });
+    }
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).json({ message: 'Missing username or password' });
+    }
+    if (!req.body.email) {
+      return res.status(400).json({ message: 'Missing email' });
+    }
+
+    const isEmail = /\S+@\S+\.\S+/.test(req.body.email);
+    if (!isEmail) {
+      return res.status(400).json({ status: res.statusCode, messaage: 'Invalid email address!' });
+    }
+
+    const queryText = `
+      INSERT INTO user_account(first_name, last_name, other_names, email, password, username, registered, is_admin)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8) returning * `;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const values = [
+      req.body.firstName,
+      req.body.lastName,
+      req.body.otherNames,
+      req.body.email,
+      hashPassword, // hash(req.body.password)
+      req.body.username,
+      moment((new Date())),
+      req.body.isAdmin,
+    ];
+
+    try {
+      const { rows } = await querySendItDb(queryText, values);
+      return res.status(201).json({ status: res.statusCode, data: rows[0], message: 'User created' });
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).json({ status: res.statusCode, message: 'User with that email or username already exists' });
+      }
+
+      return res.status(412).json({ status: res.statusCode, error });
+    }
+  },
+
+  // login user
+  async login(req, res) {
+    if (!req.body.email) {
+      return res.status(401).json({ status: res.statusCode, message: 'Missing email!' });
+    }
+    if (!req.body.password) {
+      return res.status(401).json({ status: res.statusCode, message: 'Password missing!' });
+    }
+    // check if email is valid
+    const queryText = 'SELECT * FROM user_account WHERE email = $1';
+
+    try {
+      const { rows } = await querySendItDb(queryText, [req.body.email]);
+
+      if (!rows[0]) {
+        return res.status(401).json({ status: res.statusCode, message: 'Email does not exist!' });
+      }
+      // compare password
+      const match = await bcrypt.compare(req.body.password, rows[0].password);
+      if (!match) {
+        return res.status(401).json({ status: res.statusCode, message: 'Invalid password!' });
+      }
+      // Generate token
+      const payload = {
+        id: rows[0].user_id,
+        isAdmin: rows[0].is_admin,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '5d' });
+
+      return res.status(200).json({ status: res.statusCode, token, messsage: 'Login successful' });
+    } catch (error) {
+      return res.status(400).json({ status: res.statusCode, error: error.message });
+    }
+  },
+
+  async verifyToken(req, res, next) {
+    let userToken = req.headers['x-access-token'] || req.headers.authorization;
+    if (!userToken) {
+      return res.status(401).json({ status: res.statusCode, message: 'Authorization failed!' });
+    }
+    if (userToken.startsWith('Bearer ')) {
+      userToken = userToken.slice(7, userToken.length);
+    }
+
+    try {
+      const token = await jwt.verify(userToken, process.env.JWT_SECRET_KEY);
+      if (!token) {
+        return res.status(400).json({ status: res.statusCode, messaage: 'Could not verify token' });
+      }
+
+      const queryText = 'SELECT * FROM user_account WHERE user_id = $1';
+      const { rows } = await querySendItDb(queryText, [token.id]);
+      if (!rows[0]) {
+        return res.status(404).json({ status: res.statusCode, messaage: 'User with token not found' });
+      }
+      req.user = { userId: token.id, isAdmin: token.isAdmin };
+      return next();
+    } catch (error) {
+      return res.status(400).json({ status: res.statusCode, error });
+    }
+  },
+
+
+};
+
+export default authController;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'babel-polyfill';
 import bodyParser from 'body-parser';
 
+import authController from './controllers/auth';
 import userController from './controllers/user';
 import parcelController from './controllers/parcel';
 
@@ -11,67 +12,177 @@ app.set('json spaces', 4);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 const PORT = 3000;
 
+/**
+ * @api {get} /API Status
+ * @apiGroup Status
+ * @apiSuccess {String} status API Status message
+ * @apiSuccessExample {json} Success
+ *      HTTP/1.1 200 OK
+ *      {"message": "Welcome to sendit"}
+ */
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to sendit!' });
 });
 
-// create user account
+/**
+ * @api {post} /auth/signup Register a new user
+ * @apiGroup User
+ * @apiParam {String} firstName User first name
+ * @apiParam {String} lastName User last name
+ * @apiParam {String} otherNames User other names (optional)
+ * @apiParam {String} email User email
+ * @apiParam {String} password User password
+ * @apiParam {String} username User username
+ * @apiParam {Boolean} isAdmin User type
+ * @apiParamExample {json} Input
+ *   {
+ *     "firstName": "Jon",
+ *     "lastName": "Doe",
+ *     "otherNames": "Foo",
+ *     "email": "jd@yahoo.com",
+ *     "password": "some12345",
+ *     "username": "jondoe002",
+ *     "isAdmin": false
+ *   }
+ * @apiSuccess {Number} user_id User id
+ * @apiSuccess {String} first_name User first name
+ * @apiSuccess {String} last_name User last name
+ * @apiSuccess {String} other_names User other name(s)
+ * @apiSuccess {String} email User email
+ * @apiSuccess {String} password User hashed password
+ * @apiSuccess {Boolean} is_admin User type
+ * @apiSuccess {Date} registered Registration date
+ * @apiSuccessExample {json} Success
+ *   HTTP/1.1 200 OK
+ *   {
+ *     "user_id": 7,
+ *     "first_name": "Jon",
+ *     "last_name": "Doe",
+ *     "other_names": "Foo",
+ *     "email": "jd@yahoo.com",
+ *     "password": "$2b$10$csx8rFTsCbac2RBLouyD7.aSLxvbF.yRw8SjkL6a9gPavGRJsbPge",
+ *     "username": "jondoe",
+ *     "registered": "2018-11-20T08:15:44.632Z",
+ *     "is_admin": false
+ *   }
+ * @apiErrorExample {json} Sign up error
+ *   HTTP/1.1 412 Precondition Failed
+ */
 app.post('/auth/signup', (req, res) => {
-  userController.create(req, res);
+  authController.createUserAccount(req, res);
 });
 
-// login user
+/**
+ * @api {post} /auth/login Login user
+ * @apiGroup Credentials
+ * @apiParam {String} email User email
+ * @apiParam {string} password User password
+ * @apiParamExample {json} Input
+ *   {
+ *     "email": "jondoe@yahoo.com",
+ *     "password": "jd1234"
+ *   }
+ * @apiSuccess {String} token Token of the authenticated user
+ * @apiSuccessExample {json} Success
+ *   HTTP/1.1 200 OK
+ *   {"token": "header.payload.signature"}
+ * @apiErrorExample {json} Authentication error
+ *   HTTP/1.1 401 Unauthorised
+ *   {"message": "Invalid credentials"}
+ */
 app.post('/auth/login', (req, res) => {
-  userController.login(req, res);
+  authController.login(req, res);
 });
+
+// get all users
+app.get('/api/v1/users', (req, res) => {
+  userController.getUsers(req, res);
+});
+
+/**
+ * @api {get} /api/v1/users/:userId Return a valid user's data
+ * @apiGroup User
+ * @apiHeader {String} Authorisation Token of authenticated user
+ * @apiHeaderExample {json}  Header
+ *   {"Authorisation": "Bearer thehre.hsdkj08.hjhkkr0"}
+ * @apiSuccess {Number} user_id User id
+ * @apiSuccess {String} first_name User first name
+ * @apiSuccess {String} last_name User last name
+ * @apiSuccess {String} other_names User other name(s)
+ * @apiSuccess {String} email User email
+ * @apiSuccess {String} password User hashed password
+ * @apiSuccess {Boolean} is_admin User type
+ * @apiSuccess {Date} registered Registration date
+ * @apiSuccessExample {json} Success
+ *   HTTP/1.1 200 OK
+ *   {
+ *     "user_id": 7,
+ *     "first_name": "Jon",
+ *     "last_name": "Doe",
+ *     "other_names": "Foo",
+ *     "email": "jd@yahoo.com",
+ *     "password": "$2b$10$csx8rFTsCbac2RBLouyD7.aSLxvbF.yRw8SjkL6a9gPavGRJsbPge",
+ *     "username": "jondoe",
+ *     "registered": "2018-11-20T08:15:44.632Z",
+ *     "is_admin": false
+ *   }
+ * @apiErrorExample {json} Find error
+ *   HTTP/1.1 404 Not Found
+ */
+app.get('/api/v1/users/:userId', authController.verifyToken, (req, res) => {
+  userController.getUser(req, res);
+});
+
 
 // delete a user account
-app.delete('/api/v1/users/:userId', userController.verifyToken, (req, res) => {
+app.delete('/api/v1/users/:userId', authController.verifyToken, (req, res) => {
   userController.delete(req, res);
 });
 
 // create parcel
-app.post('/api/v1/parcels', userController.verifyToken, (req, res) => {
-  parcelController.create(req, res);
+app.post('/api/v1/parcels', authController.verifyToken, (req, res) => {
+  parcelController.createParcel(req, res);
 });
 
 // get all parcel delivery orders
-app.get('/api/v1/parcels', userController.verifyToken, (req, res) => {
+app.get('/api/v1/parcels', authController.verifyToken, (req, res) => {
   parcelController.getParcels(req, res);
 });
 
 // get a specific parcel delivery order
-app.get('/api/v1/parcels/:parcelId', userController.verifyToken, (req, res) => {
+app.get('/api/v1/parcels/:parcelId', authController.verifyToken, (req, res) => {
   parcelController.getParcel(req, res);
 });
 
 // Cancel a specific parcel delivery order
-app.patch('/api/v1/parcels/:parcelId/cancel', userController.verifyToken, (req, res) => {
+app.patch('/api/v1/parcels/:parcelId/cancel', authController.verifyToken, (req, res) => {
   parcelController.cancelParcel(req, res);
 });
 
 // Change the destination of a specific parcel delivery order.
-app.patch('/api/v1/parcels/:parcelId/destination', userController.verifyToken, (req, res) => {
+app.patch('/api/v1/parcels/:parcelId/destination', authController.verifyToken, (req, res) => {
   parcelController.changeParcelDestination(req, res);
 });
 
 // Fetch all parcel delivery order by a specific user
 // Only the user that created the order is allowed to access this endpoint
-app.get('/api/v1/users/:userId/parcels', userController.verifyToken, (req, res) => {
+app.get('/api/v1/users/:userId/parcels', authController.verifyToken, (req, res) => {
   parcelController.getParcelsBySpecificUser(req, res);
 });
 
 // Change the status of a specific parcel delivery order.
 // Only the Admin is allowed to access this endpoint.
-app.patch('/api/v1/parcels/:parcelId/status', userController.verifyToken, (req, res) => {
+app.patch('/api/v1/parcels/:parcelId/status', authController.verifyToken, (req, res) => {
   parcelController.changeParcelStatus(req, res);
 });
 
 // Change the present location of a specific parcel delivery order.
 // Only the Admin is allowed to access this endpoint.
-app.patch('/api/v1/parcels/:parcelId/currentLocation', userController.verifyToken, (req, res) => {
+app.patch('/api/v1/parcels/:parcelId/currentLocation', authController.verifyToken, (req, res) => {
   parcelController.changeParcelCurrentLocation(req, res);
 });
 
